bded9282531b523c63e775e5420c1b34ba2c7ad8
RaveTheN <peppereres@live.it>
2023-02-14 09:32:13 +0100

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import { Container } from "./Container";
 import { Clock } from "./Clock";
 import { LanguageContext } from "./LanguageContext";
@@ -12,59 +12,76 @@ import { MouseTracker } from "./MouseTracker";
 import { HookCounter } from "./HookCounter";
 import { LoginForm } from "./LoginForm";
 
-const todos = [
-  { id: 1, title: "Buy Milk", completed: false },
-  { id: 2, title: "Go to the movies", completed: false },
-  { id: 3, title: "Water the plants", completed: true },
-  { id: 4, title: "Sleep", completed: true },
-];
+export function App() {
+  const [showCounter, setShowCounter] = useState(true)
 
-export class App extends Component {
-  state = {
-    language: "en",
-  };
-
-  handleLanguageChange = (event) =>
-    this.setState({
-      language: event.target.value,
-    });
-
-  render() {
-    return (
-      <div>
-        <select
-          value={this.state.language}
-          onChange={this.handleLanguageChange}
-        >
-          <option value="en">ENGLISH</option>
-          <option value="it">ITALIANO</option>
-        </select>
-        <LanguageContext.Provider value={this.state.language}>
-          <Container title={<h1>My awesome Application</h1>}>
-            <Welcome name="Jimmy" />
-            <HookCounter initialValue={15}/>
-            <LoginForm />
-            <Clock />
-          </Container>
-        </LanguageContext.Provider>
-        <MouseTracker>
-          {(position) => {
-            const [x, y] = position;
-            return (
-              <div>
-                The current position is {x}, {y}
-              </div>
-            );
-          }}
-        </MouseTracker>
-
-        <Counter />
-        <MyUncontrolledForm />
-        <MyLIst names={["Jimmy", "Robert", "John", "John"]} />
-        <MyTodos todos={todos} />
-      </div>
-    );
+  function handleToggleCounter() {
+    setShowCounter((s) => !s)
   }
+  return (
+    <div>
+      <Container title={<h1>My awesome Application</h1>}>
+        <Welcome name="Jimmy" />
+        <button onClick={handleToggleCounter}>Toggle Counter</button>
+        {showCounter && <HookCounter />}
+        <LoginForm />
+        <Clock />
+      </Container>
+    </div>
+  );
 }
 
-/**/
+// const todos = [
+//   { id: 1, title: "Buy Milk", completed: false },
+//   { id: 2, title: "Go to the movies", completed: false },
+//   { id: 3, title: "Water the plants", completed: true },
+//   { id: 4, title: "Sleep", completed: true },
+// ];
+
+// export class App extends Component {
+//   state = {
+//     language: "en",
+//   };
+
+//   handleLanguageChange = (event) =>
+//     this.setState({
+//       language: event.target.value,
+//     });
+
+//   render() {
+//     return (
+//       <div>
+//         <select
+//           value={this.state.language}
+//           onChange={this.handleLanguageChange}
+//         >
+//           <option value="en">ENGLISH</option>
+//           <option value="it">ITALIANO</option>
+//         </select>
+//         <LanguageContext.Provider value={this.state.language}>
+//           <Container title={<h1>My awesome Application</h1>}>
+//             <Welcome name="Jimmy" />
+//             <HookCounter initialValue={15}/>
+//             <LoginForm />
+//             <Clock />
+//           </Container>
+//         </LanguageContext.Provider>
+//         <MouseTracker>
+//           {(position) => {
+//             const [x, y] = position;
+//             return (
+//               <div>
+//                 The current position is {x}, {y}
+//               </div>
+//             );
+//           }}
+//         </MouseTracker>
+
+//         <Counter />
+//         <MyUncontrolledForm />
+//         <MyLIst names={["Jimmy", "Robert", "John", "John"]} />
+//         <MyTodos todos={todos} />
+//       </div>
+//     );
+//   }
+// }
--- my-app/src/HookCounter.js
+++ my-app/src/HookCounter.js
@@ -1,7 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function HookCounter({initialValue = 0}){
     const [counter, setCounter] = useState(initialValue)
+
+    useEffect(() => {
+        console.log('I have mounted')
+
+        return () => {
+            console.log('\'m about to be unmounted')
+        }
+    }, [])
+
+    //anytime a value in the dependency change, the effect (the function) will be run   
+    useEffect(() => {
+        console.log(`The counter is now ${counter}`)//function
+
+        return() => {
+            console.log(`The counter was ${counter}`)//cleanup function
+        }
+    }, [counter])//Dependecy array
+
+    useEffect(() => {
+        console.log('I have rendered')
+    })//this is without dependency array, and so it will happen anytime something inside the component changes
 
     function handleCounterIncrement(){
         setCounter(c => c + 1)
